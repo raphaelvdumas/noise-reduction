df11f3a6679ae14e157ea65bd4db5c8cf6f9f37a
DumasRaphael <raphael.v.dumas@gmail.com>
2019-05-11 12:47:28 +0200

    big edit

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 import noisereduction as nr
 import os
 
-WAV_FILE = os.path.join(os.getcwd(), 'example/noisefunkguitare')
+WAV_FILE = os.getcwd() + '/example/noisefunkguitare'
 T_NOISE = 1
 
 noised_audio = nr.Wiener(WAV_FILE, T_NOISE)
--- noisereduction.py
+++ noisereduction.py
@@ -17,108 +17,169 @@ def halfwave_rectification(array):
     return halfwave
 
 
-class NoisyAudio:
+class Wiener:
+    """
+    Class made for wiener filtering based on the article "Improved Signal-to-Noise Ratio Estimation for Speech
+    Enhancement".
+    Created by Raphael Dumas.
 
-    def __init__(self, WAV_FILE):
-        super(NoisyAudio, self).__init__()
+    Reference :
+        Cyril Plapous, Claude Marro, Pascal Scalart. Improved Signal-to-Noise Ratio Estimation for Speech
+        Enhancement. IEEE Transactions on Audio, Speech and Language Processing, Institute of Electrical
+        and Electronics Engineers, 2006.
+    """
+
+    def __init__(self, WAV_FILE, T_NOISE):
+        """
+        Input :
+            WAV_FILE
+            T_NOISE : float, Time in seconds /!\ Only works if stationnary noise is at the beginning of x /!\
+        """
+        # Constants are defined here
         self.WAV_FILE = WAV_FILE
         self.FS, self.x = wav.read(self.WAV_FILE + '.wav')
-        self.SHIFT, self.NFFT = 0.5, 2**10
-        self.FRAME = int(0.02*self.FS)
+        self.NFFT, self.SHIFT, self.T_NOISE = 2**10, 0.5, T_NOISE
+        self.FRAME = int(0.02*self.FS) # Frame of 20 ms
+
+        # Computes the offset and number of frames for overlapp - add method.
         self.OFFSET = int(self.SHIFT*self.FRAME)
+
+        # Hanning window and its energy Ew
         self.WINDOW = sg.hann(self.FRAME)
         self.EW = np.sum(self.WINDOW)
 
         self.channels = range(self.x.shape[1]) if self.x.shape != (self.x.size,)  else range(1)
         length = self.x.shape[0] if len(self.channels) > 1 else self.x.size
         self.frames = range((length - self.FRAME) // self.OFFSET + 1)
-
-
-
-    def generate_wav(self, FILE_NAME, FS, data):
-        wav.write(self.WAV_FILE + FILE_NAME + '.wav', FS, data)
-
-class NoiseEstimation(NoisyAudio):
-
-    def __init__(self, TYPE):
-        super(NoiseEstimation, self).__init__()
-
-    def welchs_periodogram(self, T_NOISE):
-        Sbb = np.zeros(self.NFFT)
-        N_NOISE = int(T_NOISE*self.FS)
-        NOISE_FRAMES = (N_NOISE - self.FRAME) // self.OFFSET + 1
-        for frame in range(NOISE_FRAMES):
-            i_min, i_max = frame*self.OFFSET, frame*self.OFFSET + self.FRAME
-            x_framed = self.x[i_min:i_max, 0]*self.WINDOW
-            X = fft(x_framed, self.NFFT)
-            Sbb = frame * Sbb / (frame + 1) + np.abs(X)**2 / (frame + 1)
-        return Sbb
-
-class Wiener(NoisyAudio):
-    """
-    Class made for wiener filtering.
-
-    """
-
-    def __init__(self, WAV_FILE, T_NOISE):
-        super(Wiener, self).__init__(WAV_FILE)
-        self.Sbb = NoiseEstimation.welchs_periodogram(self, T_NOISE)
-        self.s_est = np.zeros(self.x.shape)
+        # Evaluating noise psd with n_noise
+        self.N_NOISE = int(self.T_NOISE*self.FS)
+        self.Sbb = self.welchs_periodogram()
 
     @staticmethod
     def a_posteriori_gain(SNR):
+        """
+        Function that computes the a posteriori gain G of Wiener filtering.
+            Input :
+                SNR : 1D np.array, Signal to Noise Ratio
+            Output :
+                G : 1D np.array, gain G of Wiener filtering
+        """
         G = (SNR - 1)/SNR
         return G
 
     @staticmethod
     def a_priori_gain(SNR):
+        """
+        Function that computes the a priori gain G of Wiener filtering.
+            Input :
+                SNR : 1D np.array, Signal to Noise Ratio
+            Output :
+                G : 1D np.array, gain G of Wiener filtering
+        """
         G = SNR/(SNR + 1)
         return G
 
+    def welchs_periodogram(self):
+        """
+        Estimation of the Power Spectral Density (Sbb) of the stationnary noise
+        with Welch's periodogram given prior knowledge of n_noise points where
+        speech is absent.
+            Output :
+                Sbb : 1D np.array, Power Spectral Density of stationnary noise
+        """
+        # Initialising Sbb
+        Sbb = np.zeros(self.NFFT)
+
+        # Number of frames used for the noise
+        NOISE_FRAMES = (self.N_NOISE - self.FRAME) // self.OFFSET + 1
+        for frame in range(NOISE_FRAMES):
+            i_min, i_max = frame*self.OFFSET, frame*self.OFFSET + self.FRAME
+            x_framed = fft(self.x[i_min:i_max, 0]*self.WINDOW, self.NFFT)
+            Sbbtmp = np.abs(x_framed)**2
+            Sbb = frame * Sbb / (frame + 1) + Sbbtmp / (frame + 1)
+        return Sbb
+
     def wiener(self):
+        """
+        Function that returns the estimated speech signal using overlapp - add method
+        by applying a Wiener Filter on each frame to the noised input signal.
+            Output :
+                s_est : 1D np.array, Estimated speech signal
+        """
+        # Initialising estimated signal s_est
+        s_est = np.zeros(self.x.shape)
         for channel in self.channels:
             for frame in self.frames:
+                ############# Initialising Frame ###################################
+                # Temporal framing with a Hanning window
                 i_min, i_max = frame*self.OFFSET, frame*self.OFFSET + self.FRAME
                 x_framed = self.x[i_min:i_max, channel]*self.WINDOW
-                X = fft(x_framed, self.NFFT)
-                SNR_post = (np.abs(X)**2/self.EW)/self.Sbb
-                G = Wiener.a_priori_gain(SNR_post)
-                S = X * G
-                temp_s_est = np.real(ifft(S)) * self.SHIFT
-                self.s_est[i_min:i_max, channel] += temp_s_est[:self.FRAME]
 
-        self.generate_wav('_wiener', self.FS, self.s_est/self.s_est.max())
+                # Zero padding x_framed
+                X_framed = fft(x_framed, self.NFFT)
+
+                ############# Wiener Filter ########################################
+                # Apply a priori wiener gains G to X_framed to get output S
+                SNR_post = (np.abs(X_framed)**2/self.EW)/self.Sbb
+                G = Wiener.a_priori_gain(SNR_post)
+                S = X_framed * G
+
+                ############# Temporal estimated Signal ############################
+                # Estimated signals at each frame normalized by the shift value
+                temp_s_est = np.real(ifft(S)) * self.SHIFT
+                s_est[i_min:i_max, channel] += temp_s_est[:self.FRAME]  # Truncating zero padding
+        wav.write(self.WAV_FILE+'_wiener.wav', self.FS,s_est/s_est.max() )
 
     def wiener_two_step(self):
+        """
+        Function that returns the estimated speech signals using overlapp - add method
+        by applying a Two Step Noise Reduction on each frame (s_est_tsnr) to the noised input signal (x).
+            Output :
+                s_est_tsnr, s_est_hrnr : 1D np.array, 1D np.array
+        """
+        # Typical constant used to determine SNR_dd_prio
         beta = 0.98
+
+        # Initialising output estimated signal
+        s_est_tsnr = np.zeros(self.x.shape)
+
+        # Initialising matrix to store previous values.
+        # For readability purposes, -1 represents past frame values and 0 represents actual frame values.
         S = np.zeros((2, self.NFFT), dtype='cfloat')
         for channel in self.channels:
             for frame in self.frames:
+                ############# Initialising Frame ###################################
+                # Temporal framing with a Hanning window
                 i_min, i_max = frame*self.OFFSET, frame*self.OFFSET + self.FRAME
                 x_framed = self.x[i_min:i_max, channel]*self.WINDOW
+
                 # Zero padding x_framed
                 X_framed = fft(x_framed, self.NFFT)
+
                 ############# Wiener Filter ########################################
                 # Computation of spectral gain G using SNR a posteriori
                 SNR_post = np.abs(X_framed)**2/self.EW/self.Sbb
                 G = Wiener.a_priori_gain(SNR_post)
                 S[0, :] = G * X_framed
+
                 ############# Directed Decision ####################################
                 # Computation of spectral gain G_dd using output S of Wiener Filter
                 SNR_dd_prio = beta*np.abs(S[-1, :])**2/self.Sbb + (1 - beta)*halfwave_rectification(SNR_post - 1)
                 G_dd = Wiener.a_priori_gain(SNR_dd_prio)
                 S_dd = G_dd * X_framed
+
                 ############# Two Step Noise Reduction #############################
                 # Computation of spectral gain G_tsnr using output S_dd of Directed Decision
                 SNR_tsnr_prio = np.abs(S_dd)**2/self.Sbb
                 G_tsnr = Wiener.a_priori_gain(SNR_tsnr_prio)
                 S_tsnr = G_tsnr * X_framed
+
                 ############# Temporal estimated Signal ############################
                 # Estimated signal at frame normalized by the shift value
                 temp_s_est_tsnr = np.real(ifft(S_tsnr))*self.SHIFT
-                self.s_est[i_min:i_max, channel] += temp_s_est_tsnr[:self.FRAME] # Truncating zero padding
-                ############# Update ###############################################
-                # Rolling matrix to update old values
-                S = np.roll(S, 1, axis=0)
+                s_est_tsnr[i_min:i_max, channel] += temp_s_est_tsnr[:self.FRAME] # Truncating zero padding
 
-        self.generate_wav('_wiener_two_step', self.FS, self.s_est/self.s_est.max())
+                ############# Update ###############################################
+                # Rolling matrix to update old values (Circshift in Matlab)
+                S = np.roll(S, 1, axis=0)
+        wav.write(self.WAV_FILE+'_wiener_two_step.wav', self.FS,s_est_tsnr/s_est_tsnr.max() )
