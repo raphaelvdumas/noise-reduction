5c331e7955348795bcb0be7a1670d0655a814c58
DumasRaphael <raphael.v.dumas@gmail.com>
2019-05-11 12:16:55 +0200

    Minor Edit

--- noisereduction.py
+++ noisereduction.py
@@ -24,14 +24,16 @@ class NoisyAudio:
         self.WAV_FILE = WAV_FILE
         self.FS, self.x = wav.read(self.WAV_FILE + '.wav')
         self.SHIFT, self.NFFT = 0.5, 2**10
-        self.CHANNELS = self.x.shape[1] if self.x.shape != (self.x.size,)  else 1
         self.FRAME = int(0.02*self.FS)
         self.OFFSET = int(self.SHIFT*self.FRAME)
-        length = self.x.shape[0] if self.CHANNELS > 1 else self.x.size
-        self.FRAMES = (length - self.FRAME) // self.OFFSET + 1
-
         self.WINDOW = sg.hann(self.FRAME)
         self.EW = np.sum(self.WINDOW)
+                
+        self.channels = range(self.x.shape[1]) if self.x.shape != (self.x.size,)  else range(1)
+        length = self.x.shape[0] if len(self.channels) > 1 else self.x.size
+        self.frames = range((length - self.FRAME) // self.OFFSET + 1)
+
+
 
     def generate_wav(self, FILE_NAME, FS, data):
         wav.write(self.WAV_FILE + FILE_NAME + '.wav', FS, data)
@@ -63,24 +65,24 @@ class Wiener(NoisyAudio):
         self.Sbb = NoiseEstimation.welchs_periodogram(self, T_NOISE)
 
     @staticmethod
-    def _a_posteriori_gain(SNR):
+    def a_posteriori_gain(SNR):
         G = (SNR - 1)/SNR
         return G
 
     @staticmethod
-    def _a_priori_gain(SNR):
+    def a_priori_gain(SNR):
         G = SNR/(SNR + 1)
         return G
 
     def wiener(self):
         s_est = np.zeros(self.x.shape)
-        for channel in range(self.CHANNELS):
-            for frame in range(self.FRAMES):
+        for channel in self.channels:
+            for frame in self.frames:
                 index = frame*self.OFFSET
                 x_framed = self.x[index:index + self.FRAME, channel]*self.WINDOW
                 X = fft(x_framed, self.NFFT)
                 SNR_post = (np.abs(X)**2/self.EW)/self.Sbb
-                G = Wiener._a_priori_gain(SNR_post)
+                G = Wiener.a_priori_gain(SNR_post)
                 S = X * G
                 temp_s_est = np.real(ifft(S)) * self.SHIFT
                 s_est[index:index + self.FRAME, channel] += temp_s_est[:self.FRAME]
@@ -91,8 +93,8 @@ class Wiener(NoisyAudio):
         beta = 0.98
         s_est = np.zeros(self.x.shape)
         S = np.zeros((2, self.NFFT), dtype='cfloat')
-        for channel in range(self.CHANNELS):
-            for frame in range(self.FRAMES):
+        for channel in self.channels:
+            for frame in self.frames:
                 i_min, i_max = frame*self.OFFSET, frame*self.OFFSET + self.FRAME
                 x_framed = self.x[i_min:i_max, channel]*self.WINDOW
                 # Zero padding x_framed
@@ -100,17 +102,17 @@ class Wiener(NoisyAudio):
                 ############# Wiener Filter ########################################
                 # Computation of spectral gain G using SNR a posteriori
                 SNR_post = np.abs(X_framed)**2/self.EW/self.Sbb
-                G = Wiener._a_priori_gain(SNR_post)
+                G = Wiener.a_priori_gain(SNR_post)
                 S[0, :] = G * X_framed
                 ############# Directed Decision ####################################
                 # Computation of spectral gain G_dd using output S of Wiener Filter
                 SNR_dd_prio = beta*np.abs(S[-1, :])**2/self.Sbb + (1 - beta)*halfwave_rectification(SNR_post - 1)
-                G_dd = Wiener._a_priori_gain(SNR_dd_prio)
+                G_dd = Wiener.a_priori_gain(SNR_dd_prio)
                 S_dd = G_dd * X_framed
                 ############# Two Step Noise Reduction #############################
                 # Computation of spectral gain G_tsnr using output S_dd of Directed Decision
                 SNR_tsnr_prio = np.abs(S_dd)**2/self.Sbb
-                G_tsnr = Wiener._a_priori_gain(SNR_tsnr_prio)
+                G_tsnr = Wiener.a_priori_gain(SNR_tsnr_prio)
                 S_tsnr = G_tsnr * X_framed
                 ############# Temporal estimated Signal ############################
                 # Estimated signal at frame normalized by the shift value
